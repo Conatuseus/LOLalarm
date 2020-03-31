07ef36ffe5cca7f50f6a5ff19819c82845df0574
MK S <conatuseus@gmail.com>
2020-03-31 15:34:47 +0900

    first commit

---
+++ src/main/java/com/conatuseus/oppalol/OppaLOLApplication.java
@@ -0,0 +1,13 @@
+package com.conatuseus.oppalol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OppaLOLApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OppaLOLApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/conatuseus/oppalol/demo/oppalolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conatuseus.oppalol.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class oppalolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
