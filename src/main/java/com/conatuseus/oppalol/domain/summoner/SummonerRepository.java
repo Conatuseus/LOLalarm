2d86c0c6f93744fa293114528c221b8709bc98c7
MK S <conatuseus@gmail.com>
2020-04-02 19:49:46 +0900

    feature: Summoner 추가
    소환사(Summoner)을 위한 entity, repository 추가

---
+++ src/main/java/com/conatuseus/oppalol/domain/summoner/SummonerRepository.java
@@ -0,0 +1,6 @@
+package com.conatuseus.oppalol.domain.summoner;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SummonerRepository extends JpaRepository<Summoner, Long> {
+}
