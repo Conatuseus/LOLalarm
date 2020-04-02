e6a312b643bf1319d9efec6f68cc234e5dcf3008
MK S <conatuseus@gmail.com>
2020-04-02 20:43:23 +0900

    FEATURE: #2 Summoner 검색 기능
    Summoner Name 으로 Summoner 정보 가져오기
    (소환사가 이미 존재하는지 확인 후, 존재하지 않으면 RIOT API로 호출)
    
    Squashed commit of the following:
    
    commit 7f9428f5c13fb7f03af5ebf5e0c0d80cba3bbdb3
    Author: MK S <conatuseus@gmail.com>
    Date:   Thu Apr 2 20:41:39 2020 +0900
    
        feature: SummonerName으로 Summoner 찾는 메서드 구현
    
    commit d492fb9f6422e77e8b9ab131b33b17c3e93df7ea
    Author: MK S <conatuseus@gmail.com>
    Date:   Thu Apr 2 20:41:10 2020 +0900
    
        feature: SummonerName으로 RIOT API를 사용해 정보 가져오는 메서드 구현
    
    commit 8e679c3277bb43502ffe29601b764f222ca5bec4
    Author: MK S <conatuseus@gmail.com>
    Date:   Thu Apr 2 20:40:22 2020 +0900
    
        feature: summoner Name으로 summoner 존재 확인, 찾는 메서드 추가
    
    commit 161b57fee75e610825dcc5a1b36980782cc8fdf1
    Author: MK S <conatuseus@gmail.com>
    Date:   Thu Apr 2 20:39:39 2020 +0900
    
        refactor: 공백라인 추가
    
    commit ff06c0061cd5d675c249d7d80100b24c5d0bfcb3
    Author: MK S <conatuseus@gmail.com>
    Date:   Thu Apr 2 19:49:58 2020 +0900
    
        refactor: 마지막 빈라인 추가
    
    commit 2d86c0c6f93744fa293114528c221b8709bc98c7
    Author: MK S <conatuseus@gmail.com>
    Date:   Thu Apr 2 19:49:46 2020 +0900
    
        feature: Summoner 추가
    
        소환사(Summoner)을 위한 entity, repository 추가
    
    commit 53a850760bfc2b1824c4001c703dcb3c30744c4e
    Author: MK S <conatuseus@gmail.com>
    Date:   Thu Apr 2 19:48:36 2020 +0900
    
        feature: BaseEntity 추가
    
        id, 추가시간, 수정시간을 위한 baseEntity추가
    
    commit 418c3f8156b1ca22d33116844285c0393ed0d405
    Author: MK S <conatuseus@gmail.com>
    Date:   Thu Apr 2 19:48:03 2020 +0900
    
        feature: 기본 property추가
    
    commit 917c2b8715c6761b085adb5cb2e043694aecd8ef
    Author: MK S <conatuseus@gmail.com>
    Date:   Thu Apr 2 19:46:55 2020 +0900
    
        refactor: 사용하지 않고, 사용할 dependency 삭제/추가
    
        현재 사용하지 않는 security 의존성 삭제
        사용할 starter-web 의존성 추가
    
    commit 9a6248506467f2cd7c7a34c3356aef366f6b33be
    Author: MK S <conatuseus@gmail.com>
    Date:   Thu Apr 2 19:45:34 2020 +0900
    
        feature: test를 위한 Controller의 gitignore 추가
    
        Riot API Key가 노출되지 않기 위해 Postman으로 테스트하기 위한 컨트롤러를 gitignore

---
+++ src/main/java/com/conatuseus/oppalol/service/riotservice/RiotService.java
@@ -0,0 +1,50 @@
+package com.conatuseus.oppalol.service.riotservice;
+
+import com.conatuseus.oppalol.web.dto.RiotSummonerResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Service
+public class RiotService {
+
+    private final RestTemplate restTemplate;
+
+    @Value("${riot.api.summoner}")
+    private String findSummonerApiUrl;
+
+    public RiotService(final RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public RiotSummonerResponse findSummoner(final String summonerName) {
+        UriComponents uriComponents = UriComponentsBuilder.fromHttpUrl(findSummonerApiUrl)
+            .buildAndExpand(summonerName);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set(HttpHeaders.USER_AGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36");
+        httpHeaders.set(HttpHeaders.ACCEPT_LANGUAGE, "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7");
+        httpHeaders.set(HttpHeaders.ACCEPT_CHARSET, "application/x-www-form-urlencoded; charset=UTF-8");
+        httpHeaders.set(HttpHeaders.ORIGIN, "https://developer.riotgames.com");
+        // TODO: RIOT API KEY를 private 레포에서 가져오도록 하기
+//        httpHeaders.set("X-Riot-Token",);
+
+        HttpEntity<?> httpEntity = new HttpEntity<>(httpHeaders);
+        ResponseEntity<RiotSummonerResponse> responseEntity = restTemplate.exchange(uriComponents.toUriString(), HttpMethod.GET, httpEntity, RiotSummonerResponse.class);
+
+        if (responseEntity.getStatusCode() != HttpStatus.OK) {
+            throw new IllegalArgumentException("해당 사용자를 찾을 수 없습니다.");
+        }
+        return responseEntity.getBody();
+    }
+}
