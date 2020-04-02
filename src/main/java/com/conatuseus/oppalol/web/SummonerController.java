7f9428f5c13fb7f03af5ebf5e0c0d80cba3bbdb3
MK S <conatuseus@gmail.com>
2020-04-02 20:41:39 +0900

    feature: SummonerName으로 Summoner 찾는 메서드 구현

---
+++ src/main/java/com/conatuseus/oppalol/web/SummonerController.java
@@ -0,0 +1,23 @@
+package com.conatuseus.oppalol.web;
+
+import com.conatuseus.oppalol.service.summoner.SummonerService;
+import com.conatuseus.oppalol.web.dto.SummonerResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(SummonerController.API_V_1_SUMMONERS)
+@RequiredArgsConstructor
+public class SummonerController {
+
+    public static final String API_V_1_SUMMONERS = "/api/v1/summoners";
+    private final SummonerService summonerService;
+
+    @GetMapping("/{summonerName}")
+    public ResponseEntity<SummonerResponse> findSummoner(@PathVariable String summonerName) {
+        SummonerResponse summoner = summonerService.findSummoner(summonerName);
+        return ResponseEntity.ok(summoner);
+    }
+}
